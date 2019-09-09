47e5e83f602e514e9b9cc31a8932c03510804ef2
Jason Wang <qwang216@gmail.com>
2019-09-09 03:57:28 -0400

    removed white space

--- EOSBlock/AppDelegate.swift
+++ EOSBlock/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // TODO: Coordinator Pattern
         window = UIWindow(frame: UIScreen.main.bounds)
